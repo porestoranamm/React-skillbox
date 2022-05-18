4e37acc00711836c57435365b6e629d8021f4c62
Nikita Kuznetsov <livisback@yandex.ru>
2022-05-18 12:46:10 +0300

    add module in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
 const path = require('path');
 
+const NODE_ENV = process.env
 module.exports = {
+    mode: NODE_ENV ? NODE_ENV : 'development',
     entry: path.resolve(__dirname, 'src/index.jsx'),
     output: {
         path:path.resolve(__dirname, 'dist'),
         filename: 'index.js'
+    },
+    module: {
+        rules:[{
+            test: /\.[tj]sx?$/,
+            use: ['ts-loader']
+        }]
     }
 };
