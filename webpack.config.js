622b1a8464198778d5c91c8db8e7d192a194b87b
Nikita Kuznetsov <livisback@yandex.ru>
2022-05-24 19:47:07 +0300

    create config webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
-const NODE_ENV = process.env
+const NODE_ENV = process.env.NODE_ENV;
+
 module.exports = {
     resolve: {
         extensions:['.js','.jsx','.ts','.tsx','json']
